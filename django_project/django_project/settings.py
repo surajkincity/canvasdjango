1dd07faea3ec8c9803b599d60822c87ae89b84dd
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-02-06 14:11:16 +0530

    Def

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ib3^gd2-c!g3lpxqop9ob$&mj8ppjychezp6zew*$#rmn%5hjs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
