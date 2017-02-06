9a0f40cda26ebac3c03a42b99c90046d7b6e8599
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-02-06 14:12:40 +0530

    Dew

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ib3^gd2-c!g3lpxqop9ob$&mj8ppjychezp6zew*$#rmn%5hjs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
