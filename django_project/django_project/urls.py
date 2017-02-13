46385880ad100d146aa2ea3432993dedf87db3d4
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-02-13 17:57:33 +0530

    De

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,11 +1,12 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
-from django_project.views import home
+from django_project.views import home css
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home),
-    
+    url(r'^main.css/$', css),
+
 
 ]
--- django_project/django_project/views.py
+++ django_project/django_project/views.py
@@ -6,3 +6,6 @@ from django.templatetags.static import static
 
 def home(request):
     return render(request, 'index.html', )
+
+def css(request):
+    return render(request, 'main.css', )
