99be6108f70e52876247b4e852953d8ea0c124f2
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-02-13 17:45:38 +0530

    Fe

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -99,9 +99,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/admin/css/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/admin/css/")
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 
 
 
